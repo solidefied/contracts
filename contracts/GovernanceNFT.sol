eb23712b4d8be6127a65d792a6de747ead1d0980
Hargobind Gupta <yara.developer@gmail.com>
2022-08-23 13:11:15 +0530

    Governance: Mint and AccessRole modefied.

--- contracts/GovernanceNFT.sol
+++ contracts/GovernanceNFT.sol
@@ -10,66 +10,66 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract Governor is ERC721, ERC2981, AccessControl{
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
     using Counters for Counters.Counter;
     using Strings for uint256;
-    Counters.Counter private tokenId;
+    Counters.Counter private _tokenIdCounter;
 
     uint public TOKEN_SUPPLY;
     string public baseURI;
+    address TREASURY;
 
 
-    constructor(address _owner,string memory _baseUri, uint96 _royaltyRate) ERC721("Solidefied Governor", "POWER") {
-        _setDefaultRoyalty(_owner, _royaltyRate);
-        _grantRole(ADMIN_ROLE, _owner);
+    constructor(address treasury,string memory _baseUri, uint96 _royaltyRate) ERC721("Solidefied Governor", "POWER") {
+        TREASURY = treasury;
+        _setDefaultRoyalty(TREASURY, _royaltyRate);
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         TOKEN_SUPPLY = 250;
         baseURI = _baseUri;
     }
 
 
     // to set or update default royalty fee for every token
-    function setDefaultRoyalty(address _receiver, uint96 _royaltyRate) external onlyRole(ADMIN_ROLE) {
+    function setDefaultRoyalty(address _receiver, uint96 _royaltyRate) external onlyRole(DEFAULT_ADMIN_ROLE) {
         _setDefaultRoyalty(_receiver, _royaltyRate);
     }
 
     // to set or update total token supply
-    function setTokenSupply(uint256 _tokenSupply) external onlyRole(ADMIN_ROLE) {
+    function setTokenSupply(uint256 _tokenSupply) external onlyRole(DEFAULT_ADMIN_ROLE) {
         TOKEN_SUPPLY = _tokenSupply;
     }
 
 
      // to set or update the baseUri.
-    function setBaseURI(string memory _uri) external onlyRole(ADMIN_ROLE){
+    function setBaseURI(string memory _uri) external onlyRole(DEFAULT_ADMIN_ROLE){
         baseURI = _uri;
     }
 
-    // function _baseURI() internal view virtual override returns (string memory) {
-    //     return baseURI;
-    // }
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
 
 
-    function setMinterRole(address _minter) external onlyRole(ADMIN_ROLE) {
+    function setMinterRole(address _minter) external onlyRole(DEFAULT_ADMIN_ROLE) {
         _grantRole(MINTER_ROLE, _minter);
     }
 
     // User with Minter Role can mint token by calling this function
-    function mintToken(address _receiver) external onlyRole(MINTER_ROLE) {
-        require(_receiver != address(0), "Receiver required");
-        uint _tokenId = tokenMinted();
-        require(_tokenId < TOKEN_SUPPLY, "Limit Reached");
-        _safeMint(_receiver, _tokenId);
-        tokenId.increment();
+    function mintToken(address to) external onlyRole(MINTER_ROLE) {
+         uint256 tokenId = _tokenIdCounter.current();
+        require(tokenId < TOKEN_SUPPLY, "Limit Reached");
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
     } 
 
     //to withdraw native currency(if any)
-    function withdrawFund(address destination) external onlyRole(ADMIN_ROLE) returns(bool){
+    function withdrawFund(address destination) external onlyRole(DEFAULT_ADMIN_ROLE) returns(bool){
         (bool success, ) = destination.call{value: getBalance()}("");
         return success;
     }
 // Add function to revo any ERC20 tokens sent by accident
 
-// function transferAnyERC20Tokens(address _tokenAddr, address _to, uint _amount) public onlyRole(ADMIN_ROLE) {
+// function transferAnyERC20Tokens(address _tokenAddr, address _to, uint _amount) public onlyRole(DEFAULT_ADMIN_ROLE) {
 //         IERC20(_tokenAddr).transfer(_to, _amount);
 //     }
 
@@ -91,11 +91,18 @@ contract Governor is ERC721, ERC2981, AccessControl{
 
     //total token minted
     function tokenMinted() public view returns(uint){
-        return tokenId.current();
+        return _tokenIdCounter.current();
     }
 
-    //required
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl, ERC2981) returns (bool) {
-        return ERC721.supportsInterface(interfaceId) || ERC2981.supportsInterface(interfaceId);
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, AccessControl, ERC2981)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
+
+
 }
