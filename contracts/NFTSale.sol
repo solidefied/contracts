e70283618f40c79c3bf0ecea121f2325bfd2416f
Hargobind Gupta <yara.developer@gmail.com>
2022-09-01 14:02:52 +0530

    Contract name updated

--- contracts/NFTSale.sol
+++ contracts/NFTSale.sol
@@ -6,275 +6,221 @@
 ███████║╚██████╔╝███████╗██║██████╔╝███████╗██║     ██║███████╗██████╔╝
 ╚══════╝ ╚═════╝ ╚══════╝╚═╝╚═════╝ ╚══════╝╚═╝     ╚═╝╚══════╝╚═════╝ 
 */
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.16;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-interface IERC20 {
-    function transfer(address to, uint256 value) external returns (bool);
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+interface INonStandardERC20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address owner) external view returns (uint256 balance);
+
+    /// !!! NOTICE !!! transfer does not return a value, in violation of the ERC-20 specification
+    function transfer(address dst, uint256 amount) external;
+
+    /// !!! NOTICE !!! transferFrom does not return a value, in violation of the ERC-20 specification
     function transferFrom(
-        address from,
-        address to,
-        uint256 value
-    ) external returns (bool);
+        address src,
+        address dst,
+        uint256 amount
+    ) external;
+
+    function approve(address spender, uint256 amount)
+        external
+        returns (bool success);
+
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256 remaining);
+
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 amount
+    );
 }
 
 interface IERC721 {
     function mintToken(address _receiver) external;
-
-    function hasRole(bytes32 role, address account)
-        external
-        view
-        returns (bool);
-
-    function DEFAULT_ADMIN_ROLE() external view returns (bytes32); //not available in governance NFT
-
-    function MINTER_ROLE() external view returns (bytes32);
 }
 
-contract NFTSale is ReentrancyGuard {
-    uint256 public totalSales;
+contract SingleNFTSaleV2 is ReentrancyGuard, Ownable {
+    bool public saleActive;
+    bool public whitelistingActive;
 
-    struct Sale {
-        bool active;
-        bool freeMint;
-        bool whitelistingActive;
-        uint256 nativeTokenPrice;
-        uint256 totalPurchaseTokens;
-        mapping(address => bool) whitelist;
-        mapping(address => uint256) purchasePrice;
-        mapping(uint256 => address) purchaseToken;
+    uint256 public priceInETH;
+    uint256 public priceInUSD;
+
+    address public nftContract;
+    address public TREASURY;
+    address public USDT;
+    address public USDC;
+    address public DAI;
+
+    // address public USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7; // 6 decimals
+    // address public USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // 6 decimals
+    // address public DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F; // 18 decimals
+
+    mapping(address => bool) public whitelist;
+
+    constructor(
+        address _nftContract,
+        address _treasury,
+        uint _priceInETH,
+        uint _priceInUSD,
+        address _usdtAddress,
+        address _usdcAddress,
+        address _daiAddress
+    ) {
+        nftContract = _nftContract;
+        TREASURY = _treasury;
+        priceInETH = _priceInETH;
+        priceInUSD = _priceInUSD; //e.g. $10.22 => 1022
+        USDT = _usdtAddress;
+        USDC = _usdcAddress;
+        DAI = _daiAddress;
     }
 
-    //NFT collection address => Sale details
-    mapping(address => Sale) saleDetails;
-    //User address => Token address => balance
-    mapping(address => mapping(address => uint256)) public saleTokenBalance;
-    //User address => Native token balance
-    mapping(address => uint256) public saleNativeTokenBalance;
-
-    // Only NFT contract admins can add sale
-    modifier onlyNFTContractAdmin(address _nftContract) {
+    modifier isWhitelisted() {
         require(
-            IERC721(_nftContract).hasRole(
-                IERC721(_nftContract).DEFAULT_ADMIN_ROLE(),
-                msg.sender
-            ),
-            "!NFT Contract ADMIN"
-        );
-        _;
-    }
-
-    modifier purchaseEnabled(address _nftContract) {
-        require(
-            IERC721(_nftContract).hasRole(
-                IERC721(_nftContract).MINTER_ROLE(),
-                address(this)
-            ),
-            "!MINTER"
-        );
-        require(saleDetails[_nftContract].active, "!SALE");
-        require(
-            saleDetails[_nftContract].whitelistingActive == false ||
-                saleDetails[_nftContract].whitelist[msg.sender],
+            whitelistingActive == false || whitelist[msg.sender],
             "!WHITELISTED"
         );
         _;
     }
 
-    // Only collection owner can add listing
-    // Owner can add multiple ERC20 tokens as purchase currency
-    // Prices should be w.r.t coresponding token decimals
-    function addSale(
-        address _nftContract,
-        address[] calldata _erc20Tokens,
-        uint256[] calldata _erc20Prices,
-        uint256 _nativeTokenPrice
-    ) external onlyNFTContractAdmin(_nftContract) {
-        require(
-            saleDetails[_nftContract].active == false,
-            "Collection previously listed"
-        );
-        for (uint8 i; i < _erc20Tokens.length; i++) {
-            require(
-                saleDetails[_nftContract].purchaseToken[i] == address(0),
-                "Invalid TOKEN"
-            );
-            saleDetails[_nftContract].purchaseToken[i] = _erc20Tokens[i];
-            saleDetails[_nftContract].purchasePrice[
-                _erc20Tokens[i]
-            ] = _erc20Prices[i];
-        }
-        saleDetails[_nftContract].nativeTokenPrice = _nativeTokenPrice;
-        saleDetails[_nftContract].totalPurchaseTokens = _erc20Tokens.length;
-        saleDetails[_nftContract].active = true;
-        totalSales += 1;
+    function setTreasury(address _treasury) external onlyOwner {
+        TREASURY = _treasury;
     }
 
-    // Set NFT price in ERC20 tokens
-    function setPurchaseTokenPrice(
-        address _nftContract,
-        address _erc20Token,
-        uint256 _erc20Price
-    ) external onlyNFTContractAdmin(_nftContract) {
-        bool existingToken;
-        for (uint8 i; i < saleDetails[_nftContract].totalPurchaseTokens; i++) {
-            if (saleDetails[_nftContract].purchaseToken[i] == _erc20Token) {
-                saleDetails[_nftContract].purchasePrice[
-                    _erc20Token
-                ] = _erc20Price;
-                existingToken = true;
+    function setPriceETH(uint _priceInETH) public onlyOwner {
+        priceInETH = _priceInETH;
+    }
+
+    function setPriceUSD(uint _priceInUSD) public onlyOwner {
+        priceInUSD = _priceInUSD;
+    }
+
+    function setSaleActive(bool _active) external onlyOwner {
+        saleActive = _active;
+    }
+
+    function setWhitelistingActive(bool _active) external onlyOwner {
+        whitelistingActive = _active;
+    }
+
+    function addWhitelist(address[] calldata _whitelist) external onlyOwner {
+        for (uint256 i; i < _whitelist.length; i++) {
+            whitelist[_whitelist[i]] = true;
+        }
+    }
+
+    function buyNFTWithToken(address _purchaseToken) external nonReentrant isWhitelisted {
+        require(saleActive, "!SALE");
+        require(
+            _purchaseToken == USDT ||
+                _purchaseToken == USDC ||
+                _purchaseToken == DAI,
+            "Invalid TOKEN"
+        );
+        uint amount;
+        if (_purchaseToken == USDT || _purchaseToken == USDC) {
+            amount = (priceInUSD * 10**6) / 10**2; // dividing by 10**2 for managing cents
+        } else {
+            amount = (priceInUSD * 10**18) / 10**2;  
+        }
+
+        
+        // IERC20(_purchaseToken).transferFrom(
+        //     msg.sender,
+        //     address(this),
+        //     amount
+        // );
+        _transferTokensIn(_purchaseToken, msg.sender, amount);
+        IERC721(nftContract).mintToken(msg.sender);
+    }
+
+    function _transferTokensIn(
+        address tokenAddress,
+        address from,
+        uint256 amount
+    ) private {
+        INonStandardERC20 _token = INonStandardERC20(tokenAddress);
+        _token.transferFrom(from, address(this), amount);
+        bool success;
+        assembly {
+            switch returndatasize()
+            case 0 {
+                // This is a non-standard ERC-20
+                success := not(0) // set success to true
+            }
+            case 32 {
+                // This is a compliant ERC-20
+                returndatacopy(0, 0, 32)
+                success := mload(0) // Set success = returndata of external call
+            }
+            default {
+                // This is an excessively non-compliant ERC-20, revert.
+                revert(0, 0)
             }
         }
-        if (!existingToken) {
-            saleDetails[_nftContract].purchaseToken[
-                saleDetails[_nftContract].totalPurchaseTokens
-            ] = _erc20Token;
-            saleDetails[_nftContract].purchasePrice[_erc20Token] = _erc20Price;
-            saleDetails[_nftContract].totalPurchaseTokens += 1;
+        require(success, "!TRANSFER");
+    }
+
+    function _transferTokensOut(
+        address tokenAddress,
+        address to,
+        uint256 amount
+    ) private {
+        INonStandardERC20 _token = INonStandardERC20(tokenAddress);
+        _token.transfer(to, amount);
+        bool success;
+        assembly {
+            switch returndatasize()
+            case 0 {
+                // This is a non-standard ERC-20
+                success := not(0) // set success to true
+            }
+            case 32 {
+                // This is a compliant ERC-20
+                returndatacopy(0, 0, 32)
+                success := mload(0) // Set success = returndata of external call
+            }
+            default {
+                // This is an excessively non-compliant ERC-20, revert.
+                revert(0, 0)
+            }
         }
+        require(success, "!TRANSFER");
     }
 
-    // Set NFT price in native tokens
-    function setPurchaseNativeTokenPrice(
-        address _nftContract,
-        uint256 _nativeTokenPrice
-    ) external onlyNFTContractAdmin(_nftContract) {
-        saleDetails[_nftContract].nativeTokenPrice = _nativeTokenPrice;
+    function buyNFTWithETH() external payable nonReentrant isWhitelisted {
+        require(saleActive, "!SALE");
+        require(msg.value == priceInETH, "Incorrect AMOUNT");
+        IERC721(nftContract).mintToken(msg.sender);
     }
 
-    // Set free mint
-    function setFreeMint(address _nftContract, bool _freeMint)
+    function withdrawTokens(address _erc20Token, uint _amount)
         external
-        onlyNFTContractAdmin(_nftContract)
-    {
-        saleDetails[_nftContract].freeMint = _freeMint;
-    }
-
-    // Pause feature for individual sales
-    function setSaleActive(address _nftContract, bool _active)
-        external
-        onlyNFTContractAdmin(_nftContract)
-    {
-        saleDetails[_nftContract].active = _active;
-    }
-
-    // Set whitelist addresses
-    function setWhitelist(address _nftContract, address[] calldata _whitelist)
-        external
-        onlyNFTContractAdmin(_nftContract)
-    {
-        for (uint256 i; i < _whitelist.length; i++) {
-            saleDetails[_nftContract].whitelist[_whitelist[i]] = true;
-        }
-    }
-
-    // Enable/disable whitelisting
-    function setWhitelistingActive(address _nftContract, bool _active)
-        external
-        onlyNFTContractAdmin(_nftContract)
-    {
-        saleDetails[_nftContract].whitelistingActive = _active;
-    }
-
-    // Get sale details
-    function getSaleDetails(address _nftContract)
-        external
-        view
-        returns (
-            address[] memory _purchaseToken,
-            uint256[] memory _purchasePrice,
-            uint256 _nativeTokenPrice,
-            bool _active,
-            bool _whitelistingActive
-        )
-    {
-        _purchaseToken = new address[](
-            saleDetails[_nftContract].totalPurchaseTokens
-        );
-        _purchasePrice = new uint256[](
-            saleDetails[_nftContract].totalPurchaseTokens
-        );
-        for (uint8 i; i < saleDetails[_nftContract].totalPurchaseTokens; i++) {
-            _purchaseToken[i] = saleDetails[_nftContract].purchaseToken[i];
-            _purchasePrice[i] = saleDetails[_nftContract].purchasePrice[
-                saleDetails[_nftContract].purchaseToken[i]
-            ];
-        }
-        _nativeTokenPrice = saleDetails[_nftContract].nativeTokenPrice;
-        _active = saleDetails[_nftContract].active;
-        _whitelistingActive = saleDetails[_nftContract].whitelistingActive;
-    }
-
-    // Check if whitelisted in sale
-    function checkUserWhitelisted(address _nftContract, address _userAddress)
-        external
-        view
-        returns (bool _whitelisted)
-    {
-        _whitelisted = saleDetails[_nftContract].whitelist[_userAddress];
-    }
-
-    // NFT collection owner need to give minter role to sale contract to let users purchase NFTs
-    // To purchase using ERC20 token pass ERC20 token address in _purchaseToken
-    // To purchase using native tokens send native token while _purchaseToken is ZERO_ADDR
-    // NFT contract need to expose mintToken function
-    function purchaseNFT(address _nftContract, address _purchaseToken)
-        external
-        payable
-        purchaseEnabled(_nftContract)
+        onlyOwner
         nonReentrant
     {
-        if (!saleDetails[_nftContract].freeMint) {
-            if (_purchaseToken == address(0)) {
-                require(
-                    saleDetails[_nftContract].nativeTokenPrice == msg.value,
-                    "Incorrect AMOUNT"
-                );
-                saleNativeTokenBalance[_nftContract] += msg.value;
-            } else {
-                uint256 price = saleDetails[_nftContract].purchasePrice[
-                    _purchaseToken
-                ];
-                require(price > 0, "Incorrect AMOUNT");
-                saleTokenBalance[_nftContract][_purchaseToken] += price;
-                // Customize for non standard ERC20 tokens
-                require(
-                    IERC20(_purchaseToken).transferFrom(
-                        msg.sender,
-                        address(this),
-                        price
-                    ),
-                    "!TRANSFER"
-                );
-            }
-        }
-        IERC721(_nftContract).mintToken(msg.sender);
+        _transferTokensOut(_erc20Token, TREASURY, _amount);
+        // IERC20(_erc20Token).transfer(
+        //     TREASURY,
+        //     IERC20(_erc20Token).balanceOf(address(this))
+        // );
     }
 
-    // Ony NFT admin can withdraw ERC20 tokens from NFT sale
-    function withdrawTokenPayments(
-        address _nftContract,
-        address _erc20Token,
-        address _receiver
-    ) external onlyNFTContractAdmin(_nftContract) nonReentrant {
-        require(saleTokenBalance[_nftContract][_erc20Token] > 0, "!BALANCE");
-        uint256 balance = saleTokenBalance[_nftContract][_erc20Token];
-        saleTokenBalance[_nftContract][_erc20Token] = 0;
-        IERC20(_erc20Token).transfer(_receiver, balance);
-    }
-
-    // Ony NFT admin can withdraw tokens from NFT sale
-    function withdrawNativeTokenPayments(
-        address _nftContract,
-        address _receiver
-    ) external onlyNFTContractAdmin(_nftContract) nonReentrant {
-        require(saleNativeTokenBalance[_nftContract] > 0, "!BALANCE");
-        uint256 balance = saleNativeTokenBalance[_nftContract];
-        saleNativeTokenBalance[_nftContract] = 0;
-        payable(_receiver).transfer(balance);
+    function withdrawETH() external onlyOwner nonReentrant {
+        require(address(this).balance > 0, "!BALANCE");
+        payable(TREASURY).transfer(address(this).balance);
     }
 }
