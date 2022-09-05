e8af4ece75263f7d49941871206028cdbd583ad5
Hargobind Gupta <yara.developer@gmail.com>
2022-09-05 23:33:03 +0530

    fixed buyNFTWithToken

--- contracts/TestNFTSale copy.sol
+++ contracts/TestNFTSale copy.sol
@@ -12,41 +12,41 @@ pragma solidity ^0.8.16;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
-// interface INonStandardERC20 {
-//     function totalSupply() external view returns (uint256);
+interface INonStandardERC20 {
+    function totalSupply() external view returns (uint256);
 
-//     function balanceOf(address owner) external view returns (uint256 balance);
+    function balanceOf(address owner) external view returns (uint256 balance);
 
-//     /// !!! NOTICE !!! transfer does not return a value, in violation of the ERC-20 specification
-//     function transfer(address dst, uint256 amount) external;
+    /// !!! NOTICE !!! transfer does not return a value, in violation of the ERC-20 specification
+    function transfer(address dst, uint256 amount) external;
 
-//     /// !!! NOTICE !!! transferFrom does not return a value, in violation of the ERC-20 specification
-//     function transferFrom(
-//         address src,
-//         address dst,
-//         uint256 amount
-//     ) external;
+    /// !!! NOTICE !!! transferFrom does not return a value, in violation of the ERC-20 specification
+    function transferFrom(
+        address src,
+        address dst,
+        uint256 amount
+    ) external;
 
-//     function approve(address spender, uint256 amount)
-//         external
-//         returns (bool success);
+    function approve(address spender, uint256 amount)
+        external
+        returns (bool success);
 
-//     function allowance(address owner, address spender)
-//         external
-//         view
-//         returns (uint256 remaining);
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256 remaining);
 
-//     event Transfer(address indexed from, address indexed to, uint256 amount);
-//     event Approval(
-//         address indexed owner,
-//         address indexed spender,
-//         uint256 amount
-//     );
-// }
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 amount
+    );
+}
 
 interface IERC721 {
     function mintToken(address _receiver) external;
@@ -63,7 +63,6 @@ contract NFTPrimaryMint is ReentrancyGuard, Ownable,Pausable{
     address public USDC;
     address public DAI;
     bytes32 public root;
-    uint256 public CENTS = 10**4;
 
     // address public USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7; // 6 decimals
     // address public USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // 6 decimals
@@ -82,7 +81,7 @@ contract NFTPrimaryMint is ReentrancyGuard, Ownable,Pausable{
         nftContract = _nftContract;
         TREASURY = _treasury;
         priceInETH = _priceInETH;
-        priceInUSD = _priceInUSD; //e.g. $10.22 => 102200
+        priceInUSD = _priceInUSD; //e.g. $10.22 => 1022
         USDT = _usdtAddress;
         USDC = _usdcAddress;
         DAI = _daiAddress;
@@ -122,34 +121,84 @@ contract NFTPrimaryMint is ReentrancyGuard, Ownable,Pausable{
     }
 
     function buyNFTWithToken(address _purchaseToken, bytes32[] memory proof) external whenNotPaused() nonReentrant isWhitelisted (proof) {
-        require( _purchaseToken == USDT || _purchaseToken == USDC || _purchaseToken == DAI, "Invalid Token");
-        uint amount = (priceInUSD * IERC20Metadata(_purchaseToken).decimals()) / CENTS;
-        uint oBal = IERC20Metadata(_purchaseToken).balanceOf(address(this));
-        IERC20(_purchaseToken).transferFrom(msg.sender, address(this), amount);
-        uint nBal = IERC20Metadata(_purchaseToken).balanceOf(address(this));
-        require(nBal >= oBal + amount,"Transfere Failed");
+        require(
+            _purchaseToken == USDT ||
+                _purchaseToken == USDC ||
+                _purchaseToken == DAI,
+            "Invalid TOKEN"
+        );
+        uint amount;
+        if (_purchaseToken == USDT || _purchaseToken == USDC) {
+            amount = (priceInUSD * 10**6) / 10**2; // dividing by 10**2 for managing cents
+        } else {
+            amount = (priceInUSD * 10**18) / 10**2;  
+        }
+        _transferTokensIn(_purchaseToken, msg.sender, amount);
         IERC721(nftContract).mintToken(msg.sender);
     }
 
-    function TESTINGbuyNFTWithToken(address _purchaseToken) external whenNotPaused() nonReentrant {
-        require( _purchaseToken == USDT || _purchaseToken == USDC || _purchaseToken == DAI, "Invalid Token");
-        uint amount = (priceInUSD * IERC20Metadata(_purchaseToken).decimals()) / CENTS;
-        uint oBal = IERC20Metadata(_purchaseToken).balanceOf(address(this));
-        IERC20(_purchaseToken).transferFrom(msg.sender, address(this), amount);
-        uint nBal = IERC20Metadata(_purchaseToken).balanceOf(address(this));
-        require(nBal >= oBal + amount,"Transfere Failed");
-        IERC721(nftContract).mintToken(msg.sender);
+    function _transferTokensIn(
+        address tokenAddress,
+        address from,
+        uint256 amount
+    ) private {
+        INonStandardERC20 _token = INonStandardERC20(tokenAddress);
+        _token.transferFrom(from, address(this), amount);
+        bool success;
+        assembly {
+            switch returndatasize()
+            case 0 {
+                // This is a non-standard ERC-20
+                success := not(0) // set success to true
+            }
+            case 32 {
+                // This is a compliant ERC-20
+                returndatacopy(0, 0, 32)
+                success := mload(0) // Set success = returndata of external call
+            }
+            default {
+                // This is an excessively non-compliant ERC-20, revert.
+                revert(0, 0)
+            }
+        }
+        require(success, "!TRANSFER");
     }
 
+    function _transferTokensOut(
+        address tokenAddress,
+        address to,
+        uint256 amount
+    ) private {
+        INonStandardERC20 _token = INonStandardERC20(tokenAddress);
+        _token.transfer(to, amount);
+        bool success;
+        assembly {
+            switch returndatasize()
+            case 0 {
+                // This is a non-standard ERC-20
+                success := not(0) // set success to true
+            }
+            case 32 {
+                // This is a compliant ERC-20
+                returndatacopy(0, 0, 32)
+                success := mload(0) // Set success = returndata of external call
+            }
+            default {
+                // This is an excessively non-compliant ERC-20, revert.
+                revert(0, 0)
+            }
+        }
+        require(success, "!TRANSFER");
+    }
 
     function buyNFTWithETH(bytes32[] memory proof) external payable whenNotPaused() nonReentrant isWhitelisted(proof) {
-        require(msg.value == priceInETH, "Incorrect amount");
+        require(msg.value == priceInETH, "Incorrect AMOUNT");
         IERC721(nftContract).mintToken(msg.sender);
     }
 
     function withdrawTokens(address _erc20Token, uint _amount) external onlyOwner nonReentrant whenPaused()
     {
-         IERC20(_erc20Token).transferFrom(TREASURY, address(this), _amount);
+        _transferTokensOut(_erc20Token, TREASURY, _amount);
 
     }
 
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,15 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.16",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.16",
+      },
+      {
+        version: "0.4.17",
+        settings: {},
+      },
+    ],
+  },
 };
